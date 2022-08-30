08b68102dc935e2d4fc561d95ac95d1507adb821
Deadlye <kevin.rothenbuhler@gmail.com>
2022-08-30 15:17:52 +0200

    Change port

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const express = require("express")
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/'));
 
 //Add endpoint here
 app.all('*', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
